19071a3e2b16c2943b8dbd9a9ad19243cc19c554
Vamsi Devalla <vamsidevalla@Vamsis-MacBook-Pro.local>
2020-07-08 22:31:14 -0400

    json response: responding with games list

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
-	"time"
 	"syscall"
-	"context"
+	"time"
 )
 
 func main() {
@@ -17,15 +18,15 @@ func main() {
 	defer log.Println("Server stopped")
 
 	app := http.Server{
-		Addr: "localhost:8080",
-		Handler: http.HandlerFunc(echo),
+		Addr:         "localhost:8080",
+		Handler:      http.HandlerFunc(listGames),
 		ReadTimeout:  5 * time.Second,
-		WriteTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
 	}
 
 	serverErrors := make(chan error, 1)
 
-	go func(){
+	go func() {
 		serverErrors <- app.ListenAndServe()
 	}()
 
@@ -39,7 +40,7 @@ func main() {
 		const timeout = 5 * time.Second
 		ctx, cancel := context.WithTimeout(context.Background(), timeout)
 		defer cancel()
-		
+
 		err := app.Shutdown(ctx)
 		if err != nil {
 			log.Printf("main : Graceful shutdown did not complete in %v : %v", timeout, err)
@@ -50,11 +51,35 @@ func main() {
 			log.Fatalf("main : could not stop server gracefully : %v", err)
 		}
 	}
-
-
 }
 
-func echo(w http.ResponseWriter, r *http.Request) {
-	time.Sleep(3*time.Second);
-	fmt.Fprintf(w, "you requested for %s %s", r.Method, r.URL.Path)
+type game struct {
+	Name           string `json:"name"`
+	PriceInDollars int    `json:"priceInDollars"`
+	MyRating       int    `json:"myRating"`
+}
+
+func listGames(w http.ResponseWriter, r *http.Request) {
+	gList := []game{}
+
+	if true {
+		gList = append(gList,
+			game{"Fifa20", 60, 2},
+			game{"Apex Legends", 0, 5})
+	}
+
+	data, err := json.Marshal(gList)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error while marshaling the game list", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	if _,err = w.Write(data); err != nil {
+		log.Println("Error while writing to request", err)
+	}
 }
